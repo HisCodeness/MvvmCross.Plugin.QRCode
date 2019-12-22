c44976616cd392d984e6bec04b4341f286fac136
Nicolas BARBE <nicolas.barbe@econocom.com>
2019-12-22 01:43:26 +0100

    Add Unit test project

---
+++ Tests/MvvmCross.Plugin.QrCode.UnitTest/BaseMvxQrCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvvmCross.Plugin.QrCode.UnitTest
+{
+    [TestClass]
+    public class BaseMvxQrCode
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tests/MvvmCross.Plugin.QrCode.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MvvmCross.Plugin.QrCode.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvvmCross.Plugin.QrCode.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc3eb5f4-897f-40a5-a13a-8ffa7592a5ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
