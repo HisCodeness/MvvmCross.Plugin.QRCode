b1b703ac22b449e9f7aa33a5762b706262d8fde9
Nicolas BARBE <nicolas.barbe@econocom.com>
2019-12-22 22:22:13 +0100

    Add nuspec

---
+++ Samples/MvxQrCode.iOS/Bootstrap/MvxQrCodePluginBootstrap.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platform.Plugins;
+
+namespace MvxQrCode.iOS.Bootstrap
+{
+    /// <summary>
+    /// Plugin bootstrap
+    /// </summary>
+    public class MvxQrCodePluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugin.QrCode.PluginLoader, MvvmCross.Plugin.QrCode.iOS.Plugin>
+    {
+    }
+}
