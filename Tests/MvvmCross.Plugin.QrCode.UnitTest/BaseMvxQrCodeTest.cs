ae99b5f34412c8000b3a3e93e7f49b2ee76bf1c9
Nicolas B <bn.devproject@gmail.com>
2019-12-22 13:04:24 +0100

    Add Unit tests
    * Add unit tests

---
+++ Tests/MvvmCross.Plugin.QrCode.UnitTest/BaseMvxQrCodeTest.cs
@@ -0,0 +1,101 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvvmCross.Plugin.QrCode.UnitTest.Fakes;
+
+namespace MvvmCross.Plugin.QrCode.UnitTest
+{
+    [TestClass]
+    public class BaseMvxQrCodeTest
+    {
+        [TestMethod]
+        public async Task NullResultTest()
+        {
+            AssertScanResult(await ScanExecute<MvxQrCodeNull>(), ScanStatus.Canceled);
+        }
+
+        [TestMethod]
+        public async Task DefaultResultTest()
+        {
+            AssertScanResult(await ScanExecute<MvxQrCodeDefault>(), ScanStatus.Canceled, string.Empty);
+        }
+
+        [TestMethod]
+        public async Task SuccessResultTest()
+        {
+            AssertScanResult(await ScanExecute<MvxQrCodeSuccess>(), ScanStatus.Success, MvxQrCodeSuccess.ResultLabel);
+        }
+
+        [TestMethod]
+        public async Task ErrorResultTest()
+        {
+            AssertScanResult(await ScanExecute<MvxQrCodeError>(), ScanStatus.Error, MvxQrCodeError.ErrorLabel);
+        }
+
+        /// <summary>
+        /// Execute the scan of the given type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        private async Task<ScanResult> ScanExecute<T>() where T : IMvxQrCode, new()
+        {
+            var instance = new T();
+            return await instance.Scan(default, default, default);
+        }
+
+        /// <summary>
+        /// Assert scan result
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="expectedStatus"></param>
+        private void AssertScanResult(ScanResult result, ScanStatus expectedStatus)
+        {
+            AssertScanResult(result, expectedStatus, default, default);
+        }
+
+        /// <summary>
+        /// Assert scan result
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="expectedStatus"></param>
+        private void AssertScanResult(ScanResult result, ScanStatus expectedStatus, string expectedLabel)
+        {
+            if (expectedStatus == ScanStatus.Error)
+            {
+                AssertScanResult(result, expectedStatus, default, expectedLabel);
+            }
+            else
+            {
+                AssertScanResult(result, expectedStatus, expectedLabel, default);
+            }
+        }
+
+        /// <summary>
+        /// Assert scan result
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="expectedStatus"></param>
+        private void AssertScanResult(ScanResult result, ScanStatus expectedStatus, string expectedSuccessLabel, string expectedErrorLabel)
+        {
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedStatus, result.ScanStatus);
+
+            if (expectedSuccessLabel == default)
+            {
+                Assert.IsNull(result.Result);
+            }
+            else
+            {
+                Assert.AreEqual(expectedSuccessLabel, result.Result.Text);
+            }
+
+            if (string.IsNullOrEmpty(expectedErrorLabel))
+            {
+                Assert.IsNull(result.Exception);
+            }
+            else
+            {
+                Assert.AreEqual(expectedErrorLabel, result.Exception.Message);
+            }
+        }
+    }
+}
---
+++ Tests/MvvmCross.Plugin.QrCode.UnitTest/Fakes/MvxQrCodeSuccess.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using ZXing;
+
+namespace MvvmCross.Plugin.QrCode.UnitTest.Fakes
+{
+    /// <summary>
+    /// Success override
+    /// </summary>
+    internal class MvxQrCodeSuccess : BaseMvxQrCode
+    {
+        /// <summary>
+        /// Success label returned
+        /// </summary>
+        internal const string ResultLabel = "Success";
+
+        /// <summary>
+        /// Returns success
+        /// </summary>
+        /// <param name="topText"></param>
+        /// <param name="bottomText"></param>
+        /// <param name="cameraUnsupportedMessage"></param>
+        /// <returns></returns>
+        protected override async Task<Result> ScanNative(string topText, string bottomText, string cameraUnsupportedMessage)
+        {
+            await Task.Delay(1);
+            return new ZXing.Result(ResultLabel, new byte[0], null, ZXing.BarcodeFormat.QR_CODE);
+        }
+    }
+}
