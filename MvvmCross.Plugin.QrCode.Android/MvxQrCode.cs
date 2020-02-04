80480e56b6effb48be7384b5f3cde65f17d0389f
ArnaudMarcille <arnaud.marcille@hotmail.fr>
2020-02-04 11:01:51 +0100

    Add camera resolution Selector (#11)
    * Add resolution selection on Android

--- MvvmCross.Plugin.QrCode.Android/MvxQrCode.cs
+++ MvvmCross.Plugin.QrCode.Android/MvxQrCode.cs
@@ -1,7 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Android.Content.Res;
 using MvvmCross.Platform;
 using MvvmCross.Platform.Droid.Platform;
 using ZXing.Mobile;
+using static ZXing.Mobile.MobileBarcodeScanningOptions;
 
 namespace MvvmCross.Plugin.QrCode.Android
 {
@@ -36,14 +41,44 @@ namespace MvvmCross.Plugin.QrCode.Android
 
             try
             {
+                // Add option for use correct resolution for mobile
+                var options = new MobileBarcodeScanningOptions()
+                {
+                    CameraResolutionSelector = new CameraResolutionSelectorDelegate(ResolutionSelector)
+                };
+
                 // Start scanning
-                return await scanner.Scan();
+                return await scanner.Scan(options);
             }
             finally
             {
+
                 // Uninitialize the scanner to avoid memory leaks
                 MobileBarcodeScanner.Uninitialize(currentActivity.Application);
             }
+        }
+
+        /// <summary>
+        /// Select the resolution who fit to the screen size
+        /// </summary>
+        /// <param name="availableResolutions"></param>
+        /// <returns></returns>
+        private CameraResolution ResolutionSelector(List<CameraResolution> availableResolutions)
+        {
+            // Get current width and height
+            var currentActivity = Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity;
+            var height = currentActivity.Window.DecorView.Height;
+            var width = currentActivity.Window.DecorView.Width;
+
+            //a tolerance of 0.1 should not be visible to the user
+            double aspectTolerance = 0.1;
+            var displayOrientationHeight = currentActivity.Resources.Configuration.Orientation == Orientation.Portrait ? (double)height : (double)width;
+            var displayOrientationWidth = currentActivity.Resources.Configuration.Orientation == Orientation.Portrait ? (double)width : (double)height;
+
+            //Get the target resolution
+            var targetRatio = displayOrientationHeight / displayOrientationWidth;
+            return availableResolutions.FirstOrDefault(r => (Math.Abs((double)r.Width / r.Height) - targetRatio) < aspectTolerance);
+
         }
     }
 }
